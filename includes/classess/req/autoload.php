df83ef319755f39dcbafea98d5b6f3a00d97c66d
Shameem Reza <codechef@yahoo.com>
2019-08-20 13:52:26 +0600

    Modified :bulb:

---
+++ includes/classess/req/autoload.php
@@ -0,0 +1,13 @@
+<?php
+/*  ---------------------------------------------------------------------------
+ * 	@package	: Autoload Class
+ *	@author 	: Shameem Reza
+ *	@version	: 0.0.1
+ *	@link		: https://shameem.me
+ *	--------------------------------------------------------------------------- */
+
+require('fpdf/tfpdf.php');
+
+require('rotation/rotation.php');
+
+?>
